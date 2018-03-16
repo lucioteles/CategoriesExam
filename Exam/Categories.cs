ad2dd93e218140747d00df00c4f4e8b928e7ee1d
lucioteles <lucioteles@outook.com>
2018-03-16 13:59:36 -0300

    Final version

---
+++ Exam/Categories.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    public class Categories
+    {
+        public int CategoryId { get; set; }
+        public int ParentCategoryId { get; set; }
+        public string Name { get; set; }
+        public string Keyword { get; set; }
+    }
+}
